acd83b84c35bd8d137727fc22c9144f9eba1435e
Salah Ahmed <riddenariel@gmail.com>
2024-02-16 01:30:21 +0200

    Updated javascript
    Removed eval() function and wrote seperate math logic for each button

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,57 @@
-// Calculator
+const display = document.querySelector("#display");
+const clearButton = document.querySelector(".clear-btn");
+const numberButtons = document.querySelectorAll(".numBtn");
+const calculateButton = document.querySelector(".calculate");
+let currentEquation;
+let nextEquation;
+let currentOperator;
 
-
-const display = document.getElementById("display");
-
- 
-function appendToDisplay(input){
-    
-    if (display.value == "Error!") {
-        clearDisplay()
-    }
-    display.value += input;
+// What happens when you click on a number
+for (button of numberButtons) {
+	button.addEventListener("click", (e) => {
+		if (display.value == Infinity || display.value == NaN) {
+			display.value = "";
+		}
+		display.value = display.value + e.target.textContent;
+	});
 }
 
-
-function clearDisplay() {
-    display.value = "";
+// What happens when you click on an operator
+const mathButtons = document.querySelectorAll(".operator");
+for (button of mathButtons) {
+	button.addEventListener("click", (e) => {
+		currentOperator = e.target.textContent;
+		currentEquation = Number(display.value);
+		display.value = "";
+	});
 }
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    }
-    
-    catch(error) {
-        display.value = "Error!";
-    }
-}
+// What happens when you click on the clear button
+clearButton.addEventListener("click", () => {
+	display.value = "";
+	currentEquation = null;
+	nextEquation = null;
+	currentOperator = null;
+});
+
+// What happens when you click on calculate button
+calculateButton.addEventListener("click", () => {
+	nextEquation = Number(display.value);
+	calculate(currentEquation, nextEquation, currentOperator);
+	currentEquation = display.value;
+	nextEquation = null;
+	currentOperator = null;
+});
+
+// Calculate function
+const calculate = function (currentEquation, nextEquation, currentOperator) {
+	if (currentOperator == "+") {
+		display.value = currentEquation + nextEquation;
+	} else if (currentOperator == "-") {
+		display.value = currentEquation - nextEquation;
+	} else if (currentOperator == "*") {
+		display.value = currentEquation * nextEquation;
+	} else if (currentOperator == "/") {
+		display.value = currentEquation / nextEquation;
+	}
+};
